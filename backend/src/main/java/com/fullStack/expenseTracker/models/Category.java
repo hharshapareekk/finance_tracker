bfa60dcbc8f528ef7b61ba994fbe99ae49683dfa
shravani nomulwar <shravaninomulwar@shravanis-MacBook-Air.local>
2025-04-20 17:06:59 +0530

    Initial commit

---
+++ backend/src/main/java/com/fullStack/expenseTracker/models/Category.java
@@ -0,0 +1,34 @@
+package com.fullStack.expenseTracker.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categories")  // 👈 Add this line
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer categoryId;
+
+    private String categoryName;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "transactionTypeId")
+    private TransactionType transactionType;
+
+    private boolean enabled;
+
+
+    public Category(String categoryName, TransactionType transactionType, boolean enabled) {
+        this.categoryName = categoryName;
+        this.transactionType = transactionType;
+        this.enabled = enabled;
+    }
+}
