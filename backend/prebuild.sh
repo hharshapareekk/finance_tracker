a88938b1daa5d57b9a698a4f6eb109073409a104
Harsha Pareek <harshapareek14@gmail.com>
2025-04-22 02:10:25 +0530

    Add fixed prebuild script

--- backend/prebuild.sh
+++ backend/prebuild.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
-echo "----- Checking for Maven Wrapper -----"
-if [ ! -f "./mvnw" ]; then
-  echo "Maven wrapper missing - downloading..."
-  curl -sSL https://repo.maven.apache.org/maven2/io/takari/maven-wrapper/0.6.5/maven-wrapper-0.6.5.tar.gz | tar xz
-  chmod +x mvnw
-else
-  echo "Maven wrapper exists"
-fi
+cd backend
+(
+echo #!/bin/sh
+echo echo "----- Checking for Maven Wrapper -----"
+echo if [ ! -f "./mvnw" ]; then
+echo   echo "Maven wrapper missing - downloading..."
+echo   curl -sSL https://repo.maven.apache.org/maven2/io/takari/maven-wrapper/0.6.5/maven-wrapper-0.6.5.tar.gz ^| tar xz
+echo   chmod +x mvnw
+echo else
+echo   echo "Maven wrapper exists"
+echo fi
+) > prebuild.sh
